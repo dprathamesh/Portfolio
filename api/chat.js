3bb891e60b496b41ebf7fde21a3091779570e7eb
Prathamesh Damle <49363351+dprathamesh@users.noreply.github.com>
2025-07-16 15:12:26 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+    console.log('API Key:', process.env.GROQ_API_KEY);
     // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
